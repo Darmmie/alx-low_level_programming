a72c626e4b22800f3547a8775389b771885bf329
Darmmie <damilolaosin@hotmail>
2022-09-13 13:38:30 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,20 @@
  */
 int main(void)
 {
-	int i;
+	char a = 'a';
 
-	for (i = 48; i < 58; i++)
-		putchar(i);
-	for (i = 'a'; i < = 'f'; i++)
-		putchar(i);
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	a = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
 	putchar('\n');
 	return (0);
 }
